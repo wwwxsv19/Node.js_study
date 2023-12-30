b6e0bd75edc47517c4bc488fd3340bf27b51613d
wwwxsv19 <wwwxsv19@gmail.com>
2023-12-30 23:28:05 +0900

    ~1230

---
+++ 03_p112.js
@@ -0,0 +1,42 @@
+// p.109 코드 동적 변경하기 - user
+
+const http = require("http");
+const url = require("url");
+
+http.createServer((req, res) => {
+    const path = url.parse(req.url, true).pathname;
+    res.setHeader("Content-Type", "text/html");
+
+    if(path in urlMap) {
+        urlMap[path](req, res);
+    } else {
+        notFound(req, res);
+    }
+}).listen("3000", () => console.log("OK 서버 시작!"));
+
+
+const user = (req, res) => {
+    const userInfo = url.parse(req.url, true).query; // in 연산자를 활용해서 객체의 키 유무 조사 후 반환
+    res.end(`[user] name : ${userInfo.name}, age: ${userInfo.age}`);  // 반한된 키로 함수 실행
+};
+
+const feed = (req, res) => {
+    res.end(`<ul>
+        <li>picture1</li>
+        <li>picture2</li>
+        <li>picture3</li>
+        </ul>`
+    );
+};
+
+const notFound = (req, res) => {
+    res.statuscode = 404;
+        res.end("404 page not found");
+};
+
+
+const urlMap = { // 파이썬의 딕셔너리, 키와 값 매칭
+    "/" : (req, res) => res.end("HOME"),
+    "/user" : user,
+    "/feed" : feed,
+}
