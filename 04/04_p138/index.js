218fd8275ce06c7209e2907e44258286366e8d1a
wwwxsv19 <wwwxsv19@gmail.com>
2024-01-11 23:55:22 +0900

    ~0111 fix : 04 하위 폴더 이름

---
+++ 04/04_p138/index.js
@@ -0,0 +1,9 @@
+const calc = require("test1");
+
+const a = 17;
+const b = 3;
+
+console.log("a + b = ", calc.add(a, b));
+console.log("a - b = ", calc.sub(a, b));
+console.log("a * b = ", calc.multi(a, b));
+console.log("a / b = ", calc.div(a, b));
